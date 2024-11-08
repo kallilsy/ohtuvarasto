7dce9aa157c8ff38ff4f9908a2d8119b15179d51
kallilsy <lasse.s.kalliokoski@student.jyu.fi>
2024-11-08 16:27:58 +0200

    Muutoksia tehty testataan

--- src/varasto.py
+++ src/varasto.py
@@ -16,7 +16,7 @@ class Varasto:
             # täyteen ja ylimäärä hukkaan!
             self.saldo = tilavuus
 
-    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms. joku muutos tehty
+    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms. Näin!
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
